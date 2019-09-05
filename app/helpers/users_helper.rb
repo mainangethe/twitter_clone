fc4e074fd60d3ab585138648f193781189209c8e
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-09-05 10:52:31 +0300

    Add Signup process
    - Allows a user to register an account in the system
    - Tests to confirm valid & invalid registration process
    - Profile Image(Gravatar) helper function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,3 +1,11 @@
-# :nodoc:
+# All User Controller specific methods
 module UsersHelper
+
+  # Returns the Gravatar for the given user
+  def gravatar_for(user, image_size:80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{image_size}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+  end
+
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup info' do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post signup_path, params: {
+        user: {
+          name: '',
+          email: 'user@example',
+          password: 'dude',
+          password_confirmation: 'sup?'
+        }
+      }
+    end
+
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test 'valid signup ' do 
+    get signup_path
+
+    assert_difference 'User.count' do
+      post signup_path, params: {
+        user: {
+          name: 'Kiraitu Murungi',
+          email: 'kmurungi@example.com',
+          password: '123456',
+          password_confirmation: '123456'
+        }
+      }
+      follow_redirect!
+      assert_template 'users/show'
+      assert_not flash.empty?
+    end
+  end
+
+end
